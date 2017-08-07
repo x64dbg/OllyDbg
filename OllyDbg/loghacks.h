7d93f0a95043a2713ff659a457eae6c1517ddfed
mrexodia <mr.exodia.tpodt@gmail.com>
2017-08-07 20:43:10 +0200

    added OllyDbg plugin to see the events

--- OllyDbg/loghacks.h
+++ OllyDbg/loghacks.h
@@ -1,28 +1,6 @@
 #pragma once
 
-static inline void oprintf(const char* format, ...)
-{
-    va_list args;
-    va_start(args, format);
-    static char dprintf_msg[66000];
-    *dprintf_msg = 0;
-    vsnprintf_s(dprintf_msg, sizeof(dprintf_msg), format, args);
-    auto hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    DWORD w;
-    WriteFile(hOut, dprintf_msg, strlen(dprintf_msg), &w, nullptr);
-}
-
-static inline void oputs(const char* text)
-{
-    oprintf("%s\n", text);
-}
-
-static inline void oprintf_args(_In_z_ _Printf_format_string_ const char* Format, va_list Args)
-{
-    char buffer[16384];
-    vsnprintf_s(buffer, _TRUNCATE, Format, Args);
-    oprintf("%s", buffer);
-}
+#include "oprintf.h"
 
 #define ulog(function, ...) \
     { \
---
+++ OllyDbgEvents/OllyDbgEvents.cpp
@@ -0,0 +1,97 @@
+#include <windows.h>
+#include "../OllyDbg/oprintf.h"
+
+#define EXPORT extern "C" __declspec(dllexport)
+
+typedef unsigned long ulong;
+
+EXPORT int _ODBG_Plugindata(char* shortname)
+{
+    AllocConsole();
+    oprintf("%s()\n", __FUNCTION__);
+    strcpy_s(shortname, 32, "OllyDbgEvents");
+    return 110;
+}
+
+EXPORT int _ODBG_Plugininit(int ollydbgversion, HWND hw, ulong* features)
+{
+    oprintf("%s(%d, %X, %p)\n", __FUNCTION__, ollydbgversion, hw, features);
+    return 0;
+}
+
+EXPORT void _ODBG_Pluginmainloop(DEBUG_EVENT* debugevent)
+{
+    if(!debugevent)
+    {
+        static auto previousMillis = 0;
+        auto currentMillis = GetTickCount();
+        if(currentMillis - previousMillis >= 10000)
+            previousMillis = currentMillis;
+        else
+            return;
+    }
+    oprintf("%s(%p)\n", __FUNCTION__, debugevent);
+}
+
+EXPORT void _ODBG_Pluginsaveudd(/*t_module*/ void* pmod, int ismainmodule)
+{
+    oprintf("%s(%p, %d)\n", __FUNCTION__, pmod, ismainmodule);
+}
+
+EXPORT int _ODBG_Pluginuddrecord(/*t_module*/ void* pmod, int ismainmodule, ulong tag, ulong size, void* data)
+{
+    oprintf("%s(%p, %d, %X, %d, %p)\n", __FUNCTION__, pmod, ismainmodule, tag, size, data);
+    return 0;
+}
+
+EXPORT int _ODBG_Pluginmenu(int origin, char data[4096], void* item)
+{
+    oprintf("%s(%d, %p, %p)\n", __FUNCTION__, origin, data, item);
+    return 0;
+}
+
+EXPORT void _ODBG_Pluginaction(int origin, int action, void* item)
+{
+    oprintf("%s(%d, %d, %p)\n", __FUNCTION__, origin, action, item);
+}
+
+EXPORT int _ODBG_Pluginshortcut(int origin, int ctrl, int alt, int shift, int key, void* item)
+{
+    oprintf("%s(%d, %d, %d, %d, %d, %p)\n", __FUNCTION__, origin, ctrl, alt, shift, key, item);
+    return 0;
+}
+
+EXPORT void _ODBG_Pluginreset()
+{
+    oprintf("%s()\n", __FUNCTION__);
+}
+
+EXPORT int _ODBG_Pluginclose()
+{
+    oprintf("%s()\n", __FUNCTION__);
+    return 0;
+}
+
+EXPORT void _ODBG_Plugindestroy()
+{
+    oprintf("%s()\n", __FUNCTION__);
+    system("pause");
+}
+
+EXPORT int _ODBG_Paused(int reason, /*t_reg*/ void* reg)
+{
+    oprintf("%s(%d, %p)\n", __FUNCTION__, reason, reg);
+    return 0;
+}
+
+EXPORT int _ODBG_Pausedex(int reason, int extdata, /*t_reg*/ void* reg, DEBUG_EVENT* debugevent)
+{
+    oprintf("%s(%d, %d, %p, %p)\n", __FUNCTION__, reason, extdata, reg, debugevent);
+    return 0;
+}
+
+EXPORT int _ODBG_Plugincmd(int reason, /*t_reg*/ void* reg, char* cmd)
+{
+    oprintf("%s(%d, %p, \"%s\")\n", __FUNCTION__, reason, reg, cmd);
+    return 0;
+}
