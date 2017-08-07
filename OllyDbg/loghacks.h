c498d0a69874054f38f2ffd4c3e317c9d0f71741
mrexodia <mr.exodia.tpodt@gmail.com>
2017-08-07 05:13:22 +0200

    better loghacks

--- OllyDbg/loghacks.h
+++ OllyDbg/loghacks.h
@@ -31,49 +31,113 @@ static inline void oprintf_args(_In_z_ _Printf_format_string_ const char* Format
         oprintf(") = UNIMPLEMENTED\n"); \
     }
 
-static inline void processArg(char* x)
+#define plog(function, ...) \
+    { \
+        oprintf("%s(\n", function); /* log the function name */\
+        logArgs(__VA_ARGS__); /* log the function arguments */\
+        oprintf(")\n"); \
+    }
+
+static inline void printArg(char* x)
 {
     oprintf("\"%s\"", x);
 }
 
-static inline void processArg(const char* x)
+static inline void printArg(const char* x)
 {
     oprintf("\"%s\"", x);
 }
 
-static inline void processArg(float x)
+static inline void printArg(float x)
 {
     oprintf("%f", x);
 }
 
-static inline void processArg(double x)
+static inline void printArg(double x)
 {
     oprintf("%f", x);
 }
 
-static inline void processArg(long x)
+static inline void printArg(long x)
 {
     oprintf("%d", x);
 }
 
-static inline void processArg(int x)
+static inline void printArg(int x)
 {
     oprintf("%d", x);
 }
 
-static inline void processArg(uchar* x)
+static inline void printArg(uchar* x)
 {
     oprintf("%p", x);
 }
 
-static inline void processArg(uint x)
+static inline void printArg(uint x)
 {
     oprintf("%p", x);
 }
 
-static inline void processArg(ulong x)
+static inline void printArg(ulong x)
 {
     oprintf("%p", x);
+}
+
+static inline void printArg(t_asmmodel [])
+{
+    oprintf("TODO: t_asmmodel");
+}
+
+static inline void printArg(const void* x)
+{
+    oprintf("%p", x);
+}
+
+#include <functional>
+
+template<typename Arg>
+using CustomPrint = std::function<void(const char* name, const char* format, Arg value)>;
+
+template<typename Arg>
+struct ArgHack
+{
+    const char* name = nullptr;
+    const char* format = nullptr;
+    Arg value;
+    CustomPrint<Arg> customPrint;
+
+    ArgHack() { }
+
+    ArgHack(const char* name, Arg value)
+        : name(name), value(value) { }
+
+    ArgHack(const char* name, const char* format, Arg value)
+        : name(name), format(format), value(value) { }
+
+    ArgHack(const char* name, Arg value, const CustomPrint<Arg> & customPrint)
+        : name(name), value(value), customPrint(customPrint) { }
+
+    void Print() const
+    {
+        if(customPrint)
+        {
+            customPrint(name, format, value);
+            return;
+        }
+
+        if(name)
+            oprintf("%s: ", name);
+        if(format)
+            oprintf(format, value);
+        else
+            printArg(value);
+    }
+};
+
+template<typename Arg>
+static inline void printArg(const ArgHack<Arg> & hack)
+{
+    hack.Print();
 }
 
 //no arguments base case
@@ -81,14 +145,32 @@ static inline void logArgs()
 {
 }
 
-#define p(x) #x, x
+template<typename Arg>
+ArgHack<Arg> argHack(const char* name, Arg value)
+{
+    return ArgHack<Arg>(name, value);
+}
+
+template<typename Arg>
+ArgHack<Arg> argHack(const char* name, const char* format, Arg value)
+{
+    return ArgHack<Arg>(name, format, value);
+}
+
+template<typename Arg>
+ArgHack<Arg> argHack(const char* name, Arg value, const CustomPrint<Arg> & customPrint)
+{
+    return ArgHack<Arg>(name, value, customPrint)
+}
+
+#define p(x) argHack(#x, x)
 
 //template takes one argument (Arg) and zero or more extra arguments (Args)
 template<typename Arg, typename... Args>
-static inline void logArgs(const char* a1_name, Arg a1, Args... args)
+static inline void logArgs(Arg a1, Args... args)
 {
-    oprintf("  %s: ", a1_name);
-    processArg(a1); //call processArg on the current (first) argument
+    oprintf("  ");
+    printArg(a1); //call printArg on the current (first) argument
     oprintf("\n");
     logArgs(args...); //expand the additional arguments -> logArgs(a2, a3, a4) where args = { a2, a3, a4 }
 }
