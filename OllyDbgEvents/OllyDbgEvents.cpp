6d0005eaaadf5e8c68e5041e8464c9deb50bc9ee
mrexodia <mr.exodia.tpodt@gmail.com>
2017-08-11 04:12:10 +0200

    more tests in OllyDbgEvents

--- OllyDbgEvents/OllyDbgEvents.cpp
+++ OllyDbgEvents/OllyDbgEvents.cpp
@@ -1,9 +1,13 @@
 #include <windows.h>
 #include "../OllyDbg/oprintf.h"
+#define _CHAR_UNSIGNED //TODO: this is probably harmful for the implementation of certain functions
+#include "../OllyDbg/OllyDbg.h"
+
+#pragma comment(lib, "OllyDbg.lib")
 
 #define EXPORT extern "C" __declspec(dllexport)
 
-typedef unsigned long ulong;
+static bool isRealOlly = false;
 
 EXPORT int _ODBG_Plugindata(char* shortname)
 {
@@ -29,6 +33,7 @@ EXPORT int _ODBG_Plugininit(int ollydbgversion, HWND hw, ulong* features)
             VirtualProtect(addr, 0x1, PAGE_EXECUTE_READWRITE, &old);
             *(unsigned char*)addr = 0xEB;
             VirtualProtect(addr, 0x1, old, &old);
+            isRealOlly = true;
         }
     }
     __except(EXCEPTION_EXECUTE_HANDLER)
@@ -51,14 +56,30 @@ EXPORT void _ODBG_Pluginmainloop(DEBUG_EVENT* debugevent)
     oprintf("%s(%p)\n", __FUNCTION__, debugevent);
 }
 
-EXPORT void _ODBG_Pluginsaveudd(/*t_module*/ void* pmod, int ismainmodule)
+#define UDD_TAG 1337
+
+EXPORT void _ODBG_Pluginsaveudd(t_module* pmod, int ismainmodule)
 {
-    oprintf("%s(%p, %d)\n", __FUNCTION__, pmod, ismainmodule);
+    oprintf("%s(\"%s\", %d)\n", __FUNCTION__, pmod->path, ismainmodule);
+    DWORD ms = GetTickCount();
+    oprintf("Pluginsaverecord(%u): %d\n", ms, Pluginsaverecord(UDD_TAG, sizeof(ms), &ms));
 }
 
-EXPORT int _ODBG_Pluginuddrecord(/*t_module*/ void* pmod, int ismainmodule, ulong tag, ulong size, void* data)
+EXPORT int _ODBG_Pluginuddrecord(t_module* pmod, int ismainmodule, ulong tag, ulong size, void* data)
 {
     oprintf("%s(%p, %d, %X, %d, %p)\n", __FUNCTION__, pmod, ismainmodule, tag, size, data);
+    if(tag == UDD_TAG)
+    {
+        DWORD ms;
+        if(size == sizeof(ms))
+        {
+            memcpy(&ms, data, sizeof(ms));
+            oprintf("data: %u\n", ms);
+        }
+        else
+            oprintf("INVALID UDD DATA!");
+        return 1;
+    }
     return 0;
 }
 
@@ -95,22 +116,23 @@ EXPORT int _ODBG_Pluginclose()
 EXPORT void _ODBG_Plugindestroy()
 {
     oprintf("%s()\n", __FUNCTION__);
-    system("pause");
+    if(isRealOlly)
+        system("pause");
 }
 
-EXPORT int _ODBG_Paused(int reason, /*t_reg*/ void* reg)
+EXPORT int _ODBG_Paused(int reason, t_reg* reg)
 {
     oprintf("%s(%d, %p)\n", __FUNCTION__, reason, reg);
     return 0;
 }
 
-EXPORT int _ODBG_Pausedex(int reason, int extdata, /*t_reg*/ void* reg, DEBUG_EVENT* debugevent)
+EXPORT int _ODBG_Pausedex(int reason, int extdata, t_reg* reg, DEBUG_EVENT* debugevent)
 {
     oprintf("%s(%d, %d, %p, %p)\n", __FUNCTION__, reason, extdata, reg, debugevent);
     return 0;
 }
 
-EXPORT int _ODBG_Plugincmd(int reason, /*t_reg*/ void* reg, char* cmd)
+EXPORT int _ODBG_Plugincmd(int reason, t_reg* reg, char* cmd)
 {
     oprintf("%s(%d, %p, \"%s\")\n", __FUNCTION__, reason, reg, cmd);
     return 0;
